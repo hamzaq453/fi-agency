7f408394c0e72f3dab04987e06cfe9336e1e8417
Hamza <qhamza4532@gmail.com>
2024-09-16 18:40:35 +0500

    up

--- src/app/components/Brands.tsx
+++ src/app/components/Brands.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Logo from '../../../public/Logo.jpg';
 import BrandZA from '../../../public/BrandZA.png';
 import BrandOXO from '../../../public/BrandOXO.jpg';
 import BrandRC from '../../../public/BrandRC.jpg';
